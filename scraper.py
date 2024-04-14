87ebef26f65ff38a4ff2a27a013ba1a750fbafa4
Samuel Odongo <samieodd@gmail.com>
2024-04-14 07:15:52 +0300

    Complete scraper

--- scraper.py
+++ scraper.py
@@ -1,11 +1,18 @@
-from typing import List
+from typing import Dict, List
 import pyppeteer
 from pyppeteer.browser import Browser
 from pyppeteer.page import Page, ElementHandle
+from pyppeteer.frame_manager import Frame
+from pyppeteer.execution_context import JSHandle
 
 
 class Scraper:
     link: str = 'https://spela.svenskaspel.se/odds/sports/ishockey/sverige-hockeyallsvenskan'
+    matches: Dict[str, Dict[str, List[Dict[str, Dict[str, str]]]]] = {
+        'svenskaspel': {
+            'match': [],
+        }
+    }
 
     def __init__(self, link: str) -> None:
         # Set link to one provided by user
@@ -24,11 +31,58 @@ class Scraper:
     async def getData(self) -> None:
         await self.browserSetup()
         await self.page.goto(url=self.link)
-        title: str = await self.page.title()
-        print('-> ', title)
+
         acceptCookieButton: ElementHandle | None = await self.page.querySelector('#onetrust-accept-btn-handler')
         if acceptCookieButton:
             await acceptCookieButton.click()
-        self.listingElements: List[ElementHandle] = await self.page.querySelectorAll('#eventsWrapper-Center_LeagueViewResponsiveBlock_15984 > sb-comp > div > sb-lazy-render > div')
-        print(len(self.listingElements))
+
+        # Target iframe holding the data
+        iframe: ElementHandle | None = await self.page.querySelector('iframe.sbtech')
+        contentFrame: Frame | None = None
+        if iframe:
+            contentFrame = await iframe.contentFrame()
+
+        if contentFrame:
+            self.listingElements: List[ElementHandle] = await contentFrame.querySelectorAll('.rj-ev-list__ev-card')
+        if self.listingElements.__len__() > 0:
+            for match in self.listingElements:
+                hometeamElement: ElementHandle | None = await match.querySelector('.rj-ev-list__ev-card__team-1-name')
+                hometeam: str = ''
+                if hometeamElement:
+                    hometeamJsonValue: JSHandle = await hometeamElement.getProperty('textContent')
+                    hometeam = await hometeamJsonValue.jsonValue()
+
+                awayteamElement: ElementHandle | None = await match.querySelector('.rj-ev-list__ev-card__team-2-name')
+                awayteam: str = ''
+                if awayteamElement:
+                    awayteamJsonValue: JSHandle = await awayteamElement.getProperty('textContent')
+                    awayteam = await awayteamJsonValue.jsonValue()
+
+                oddsElements: List[ElementHandle] = await match.querySelectorAll('.rj-ev-list__bet-btn__odd')
+                odds1: str = ''
+                oddsx: str = ''
+                odds2: str = ''
+                odds: List[str] = []
+                if len(oddsElements) == 3:
+                    for elem in oddsElements:
+                        textJsonValue: JSHandle = await elem.getProperty('textContent')
+                        text = await textJsonValue.jsonValue()
+                        odds.append(text)
+                if len(odds) == 3:
+                    odds1 = odds[0]
+                    oddsx = odds[1]
+                    odds2 = odds[2]
+
+                self.matches['svenskaspel']['match'].append({
+                    'teams': {
+                        'hometeam': hometeam,
+                        'awayteam': awayteam,
+                    },
+                    'odds': {
+                        'odds1': odds1,
+                        'oddsx': oddsx,
+                        'odds2': odds2,
+                    },
+                })
+
         await self.browserDisposal()
