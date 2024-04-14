67f76f77bdc847072d4773554f327948008a5590
Samuel Odongo <samieodd@gmail.com>
2024-04-14 08:37:52 +0300

    Write results to xml file

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from scraper import Scraper
 
 
 async def main() -> None:
-    # TODO: Prompt user
-    # TODO: Display filename when scraping is done
     scraper: Scraper = Scraper('')
     await scraper.getData()
 
+    if scraper.matches['svenskaspel']['match']:
+        import xmltodict
+        xmldata = xmltodict.unparse(scraper.matches, pretty=True)
+
+        with open('betting.xml', 'w') as xmlFile:
+            xmlFile.write(xmldata)
+
 
 if __name__ == '__main__':
+    import os
+    os.environ['PYPPETEER_CHROMIUM_REVISION'] = '1263111'
+
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
     loop.close()
